b59cce31477450eb2ad8ca09442d7aeddf008534
ypedro <ynoa.pedro@outlook.com>
2020-09-15 12:01:08 -0300

    Create post integration unit test

--- tests/partsUnitTest.js
+++ tests/partsUnitTest.js
@@ -1,17 +1,33 @@
 const chai = require('chai');
 const part = require('../routes/parts');
 const chaiHttp = require('chai-http');
+const Part = require('../models/parts');
 
 chai.use(chaiHttp);
 
-describe('Part Routing Tests', () => {
+describe('Part Integration Tests', () => {
     it("should get all parts", done => {
         chai.request(part)
             .get('/')
             .end((err, res) => {
-                res.should.have.status(201);
+                res.should.have.status(200);
                 res.body.should.be.a('json');
             });
-            done();
-        })
+        done();
+    });
+    it("should create new parts", done => {
+        partMock = new Part({
+            name: "",
+            email: "",
+            cpf: "",
+            phone: ""
+        });
+        chai.request(part)
+            .post('/')
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.a(partMock);
+            });
+        done();
+    });
 });
