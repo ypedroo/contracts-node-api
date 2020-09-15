9cf0634f98cda52037f8a3a984a7d74d7c152abd
ypedro <ynoa.pedro@outlook.com>
2020-09-15 11:52:22 -0300

    Add first integration test

--- tests/partsUnitTest.js
+++ tests/partsUnitTest.js
@@ -1,9 +1,17 @@
-const assert = require('chai').assert;
-const app = require('../routes/parts');
+const chai = require('chai');
+const part = require('../routes/parts');
+const chaiHttp = require('chai-http');
 
+chai.use(chaiHttp);
 
 describe('Part Routing Tests', () => {
-    it('Get Part Testes', () => {
-        
-    });
-})
+    it("should get all parts", done => {
+        chai.request(part)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.a('json');
+            });
+            done();
+        })
+});
