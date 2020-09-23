367ff91025a1fdd32d1aba2b312c746b2bd9797f
ypedro <ynoa.pedro@outlook.com>
2020-09-22 22:47:06 -0300

    Add doc folder and use env for port selection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
+    swaggerDocument = require('./doc/swagger.json');
 
 const app =  express();
 
@@ -21,4 +21,4 @@ app.use(express.json());
 const partsRouter = require('./routes/parts');
 app.use('/parts', partsRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(3000, () => console.log('hey mate'));
+app.listen(process.env.PORT, () => console.log('hey mate'));
